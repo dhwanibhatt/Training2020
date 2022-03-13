2e8ef3a12cd791bfac527756add3347ee284aba5
Dhwani <iamdhwani2@gmail.com>
2022-03-13 21:18:35 +0530

    MDN practice assignments uploaded

---
+++ Week3/18Feb22-MDN_Assignment/SampleWebPage/server.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const app=express()
+const port=3000;
+//app.use(express.json());
+//Use as middleware there convert data json file into body object.
+app.use(express.json());
+app.use('/css',express.static(__dirname +'/css'));
+app.use('/img',express.static(__dirname +'/img'));
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html');
+});
+app.get('/',(req,resp)=>{
+    //resp.set(“Content-Type”, “application/CSS”);
+    resp.sendFile(__dirname+'style.css')
+});
+app.get('/',(req,resp)=>{
+    //resp.set(“Content-Type”, “application/CSS”);
+    resp.sendFile(__dirname+'logo.png')
+});
+app.listen(port,()=>{
+    console.log(`http://localhost:${port} start`);
+})
